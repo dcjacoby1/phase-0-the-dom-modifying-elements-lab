7f089167f0a7683ae155d6b05bb35f8ac32896ad
Dan Jacoby <jacobydan01@gmail.com>
2024-03-29 11:48:22 -0400

    dommod

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-main.remove();
-const newHeader = document.createElement("h1");
-newHeader.id = "victory";
-newHeader.innerHTML = "Dan is the champion";
+//.remove removes the main and all of its child elements
+main.remove()
+//creates an h1 element called h1
+const newHeader = document.createElement("h1")
+//sets the id of newHeader to victory
+newHeader.id = "victory"
+//sets the textcontent equal to "Dan is the champion"
+//difference between innerHTML and textContent is that innerHTML allows for inserting HTML tags as well
+//for example an <li> can be added with innerHTML but not textContent
+newHeader.innerHTML = "Dan is the champion"
+//to add to existing: newHeader.innerHTML += "Dan is the champion"
